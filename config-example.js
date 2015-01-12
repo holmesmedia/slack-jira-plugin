721cb9f23b936a48fa65990d71c5d6bcabce1550
gsingers <gsingers@apache.org>
2015-01-12 18:57:58 -0500

    Initial checkin

---
+++ config-example.js
@@ -0,0 +1,15 @@
+***REMOVED***
+
+***REMOVED***
+
+    token: 'XXXX-XXXXXXXXXX-XXXXXXXXXX-XXXXXXXXXX-XXXXXX',
+  ***REMOVED***
+      "APACHE": "https://issues.apache.org/jira/browse/",
+      "REPLACE": "https://my.path.to.jira/browse/"
+***REMOVED***
+    pattern: /((GRANT))-\d+/g,
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
