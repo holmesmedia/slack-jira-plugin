ec5b2a35d57e1093893a02d98699428f306d5101
gsingers <gsingers@apache.org>
2015-01-12 19:08:32 -0500

    Multiple repo support

--- config-example.js
+++ config-example.js
@@ -5,9 +5,10 @@
     token: 'XXXX-XXXXXXXXXX-XXXXXXXXXX-XXXXXXXXXX-XXXXXX',
   ***REMOVED***
       "APACHE": "https://issues.apache.org/jira/browse/",
-      "REPLACE": "https://my.path.to.jira/browse/"
+      "REPLACE": "https://my.path.to.jira/browse/",
+      "DEFAULT": "https://my.path.to.jira/browse/"
 ***REMOVED***
-    pattern: /((GRANT))-\d+/g,
+    pattern: /((GRANT|BOB))-\d+/g//NOTE this assumes all JIRA issues are like: PROJECT_NAME-1234
 ***REMOVED***
 
 ***REMOVED***
