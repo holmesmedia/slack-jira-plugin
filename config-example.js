81bdc028db0f51ecb0f8bce585b644f3f1e12cb6
gsingers <gsingers@apache.org>
2015-01-13 08:19:48 -0500

    Added support for posting instead of updating

--- config-example.js
+++ config-example.js
@@ -1,14 +1,15 @@
 ***REMOVED***
 
 ***REMOVED***
-
+    bot_id: "",//Create a Bot in the Slack integrations and provide the id here
     token: 'XXXX-XXXXXXXXXX-XXXXXXXXXX-XXXXXXXXXX-XXXXXX',
   ***REMOVED***
       "APACHE": "https://issues.apache.org/jira/browse/",
       "REPLACE": "https://my.path.to.jira/browse/",
       "DEFAULT": "https://my.path.to.jira/browse/"
 ***REMOVED***
-    pattern: /((GRANT|BOB))-\d+/g//NOTE this assumes all JIRA issues are like: PROJECT_NAME-1234
+    pattern: /((GRANT|BOB))-\d+/g, //NOTE this assumes all JIRA issues are like: PROJECT_NAME-1234
+    post: true //If true, than post a new message instead of updating the current
 ***REMOVED***
 
 ***REMOVED***
