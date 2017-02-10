af50c74a5851b4bd3b8b34c6338056f4f206f27d
John Byrne <admin@intotherough.co.uk>
2017-02-10 15:58:14 +0000

    Replace token

--- config-example.js
+++ config-example.js
@@ -1,45 +1,45 @@
-***REMOVED***
+var slackbot = require('./lib/bot');
 
-***REMOVED***
+var config = {
 
-***REMOVED***
-***REMOVED***
+  showIssueDetails: true,
+  issueDetailsToShow: {'fields.summary':1 , 'fields.assignee' : 1, 'fields.creator' : 0, 'fields.description': 0},
   showDetailsByDefault: true,//if true, you don't need the '+' to get details
   bot_name: "jira",//Provide the name to post under
   token: 'XXXX-XXXXXXXXXX-XXXXXXXXXX-XXXXXXXXXX-XXXXXX', // https://api.slack.com/web
-***REMOVED***
-***REMOVED***
-***REMOVED***url: "https://default.jira.server/browse/"},
+  jira_urls: {
+    // DEFAULT NODE IS REQUIRED.
+    "DEFAULT": {url: "https://default.jira.server/browse/"},
     // These should match projects from the projects property where you want to use a configuration other than the default
     "SOLR": {
       url: "https://issues.apache.org/jira/browse/",
-***REMOVED***
+      jira: {
         user: 'username', // be sure to use the username, not the user email
         password: 'password',
         host: 'hostname',
-***REMOVED***
-***REMOVED***
+        protocol: 'https',
+        port: 443,
         version: '2',
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
+        verbose: true,
+        strictSSL: true
+      }
+    },
     "GRANT": {url:"http://grant.jira.server/jira/browse/"}
-***REMOVED***
+  },
   search_cmd: "search",
-***REMOVED***
+  //Since search results can be verbose, you may not want to muddy the channel
   search_output_chan: "C02U1L9KZ",//if the value is "this", then the current channel will be used, else the name of a channel
   projects: ["REPLACE", "ME", "WITH", "YOUR", "PROJECT", "NAMES", "GRANT", "SOLR"],
-***REMOVED***
-***REMOVED***
-***REMOVED***
+  post: true,
+  verbose: true,
+  custom_texts: {
     messagePrefix: "Hey, thought this might help: " //message you might like to prefix to JiraBot's post
-***REMOVED***
-***REMOVED***
+  },
+  emoji: ":jira:", // be sure to upload your custom emoji in slack
   link_separator: ", ",// use \n if you want new lines
   error_channel: '' //the id of the channel to send low level log errors.  If not defined, will use the current channel
-***REMOVED***
+};
 
-***REMOVED***
-***REMOVED***
-***REMOVED***
+//DO NOT EDIT BELOW HERE
+var slackbot = new slackbot.Bot(config);
+slackbot.run();
---
+++ jirabot.js
@@ -0,0 +1,46 @@
+var slackbot = require('./lib/bot');
+
+var config = {
+
+  showIssueDetails: true,
+  //showIssueDetails: true,
+  //issueDetailsToShow: {'fields.summary':1 , 'fields.assignee' : 1, 'fields.creator' : 0, 'fields.description': 0},
+  issueDetailsToShow: {'fields.summary':1 , 'fields.assignee' : 1, 'fields.creator' : 0, 'fields.description': 0},
+  showDetailsByDefault: true, // false does not work unfortunately
+  //showDetailsByDefault: true, //if true, you don't need the '+' to get details
+  bot_name: "jirabot",//Provide the name to post under
+  token: process.env.slackToken,
+  jira_urls: {
+    // DEFAULT NODE IS REQUIRED.
+    "DEFAULT": {
+      url: "https://holmesmedia.atlassian.net/browse/",
+      jira: {
+        user: process.env.jiraUser, // be sure to use the username, not the user email
+        password: process.env.jiraPass,
+        host: 'holmesmedia.atlassian.net',
+        protocol: 'https',
+        port: 443,
+        version: 'latest',
+        verbose: true,
+        strictSSL: true
+      }
+    },
+  },
+  search_cmd: "no_searching_allowed",
+  // search_cmd: "search",
+  //Since search results can be verbose, you may not want to muddy the channel
+  search_output_chan: "this",//if the value is "this", then the current channel will be used, else the name of a channel
+  projects: ["BGAR", "IT", "T3", "BG", "SR", "WG", "RG","TA","ID"],
+  post: true,
+  verbose: true,
+  custom_texts: {
+    messagePrefix: "" //message you might like to prefix to JiraBot's post
+  },
+  emoji: ":jira:", // be sure to upload your custom emoji in slack
+  link_separator: " - ",// use \n if you want new lines
+  error_channel: 'jirabot-testing' //the id of the channel to send low level log errors.  If not defined, will use the current channel
+};
+
+//DO NOT EDIT BELOW HERE
+var slackbot = new slackbot.Bot(config);
+slackbot.run();
